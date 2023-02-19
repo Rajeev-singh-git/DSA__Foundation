82ceca8fd15b05a31e0a1e586af732d254e0fc8b
Rajeev Singh <rajeevsinghece49@gmail.com>
2023-02-19 16:33:49 +0530

    Spirally traversing a matrix

---
+++ src/matrix/Spirally traversing a matrix.java
@@ -0,0 +1,40 @@
+class Solution
+{
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    static ArrayList<Integer> spirallyTraverse(int matrix[][], int r, int c)
+    {
+        int left = 0;
+        int right = c-1;
+        int top = 0;
+        int bottom = r-1;
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        while(left<=right && top<=bottom){
+            
+            for(int i=left;i<=right;i++){
+                list.add(matrix[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                list.add(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    list.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    list.add(matrix[i][left]);
+                }
+                left++;
+            }
+            
+        }
+        
+        return list;
+    }
+}
