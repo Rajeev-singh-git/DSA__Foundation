da30c8f28780ba4568b94412d9bdfd3828c21903
Rajeev Singh <rajeevsinghece49@gmail.com>
2023-10-20 18:12:29 +0530

    Pattern Searching

---
+++ src/array/array_gfg/easy/patternSearching.java
@@ -0,0 +1,39 @@
+package array.array_gfg.easy;
+
+import java.util.Scanner;
+
+public class patternSearching {
+
+    public static void main(String[]args){
+
+        Scanner scn = new Scanner(System.in);
+
+        String s1 = scn.nextLine();
+        String s2 = scn.nextLine();
+
+        checkPattern(s1,s2);
+    }
+
+    private static void checkPattern(String s1, String s2) {
+
+        int j=0;
+        int size = s2.length();
+
+
+        for(int i=0;i<s1.length();i++){
+
+            if(s1.charAt(i)==s2.charAt(j)){
+                j++;
+                if(j==size){
+                   System.out.println("Pattern Matched");
+                   break;
+                }
+            }
+        }
+
+        if(j!=size)
+        System.out.println("Pattern didn't Matched");
+
+    }
+
+}
