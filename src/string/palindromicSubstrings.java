db8ef1cd64e4ef0f1ff7f1ae1982b51b23158750
Rajeev Singh <rajeevsinghece49@gmail.com>
2024-02-08 22:32:09 +0530

    String

--- src/string/palindromicSubstrings.java
+++ src/string/palindromicSubstrings.java
@@ -6,13 +6,16 @@ class palindromicSubstrings {
 
     public  static  void main (String[]args){
         Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter String");
+
         String s = scn.nextLine();
 
         for(int i=0;i<s.length()+1;i++){
-            for(int j=i+1;j<s.length()+1;j++){
+            for(int j=i+1;j<=s.length();j++){
                String str =  s.substring(i,j);
 
-               if(isPalindrome(str)==true){
+               if(isPalindrome(str)){
                    System.out.println(str);
                }
             }
